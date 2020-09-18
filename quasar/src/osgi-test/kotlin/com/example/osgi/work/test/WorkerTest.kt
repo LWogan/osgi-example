05e5cad9eab6062bf515e745dc8c4a6dccfad4ac
Thomas Schroeter <thomas.schroeter@r3.com>
2020-09-18 10:44:38 +0100

    WIP: Integrate Corda Core

--- quasar/src/osgi-test/kotlin/com/example/osgi/work/test/WorkerTest.kt
+++ quasar/src/osgi-test/kotlin/com/example/osgi/work/test/WorkerTest.kt
@@ -1,6 +1,7 @@
 package com.example.osgi.work.test
 
 import com.example.osgi.api.Greetings
+import net.corda.core.identity.CordaX500Name
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -15,6 +16,10 @@ class WorkerTest {
 
     @Test
     fun testWork() {
+
+        val x500Name = CordaX500Name.parse("L=London,C=GB,O=TestOSGi")
+        println("x500Name = $x500Name")
+
         val name = "Charlie"
         assertEquals("You are suspended, $name!", greetings.greet(name))
     }
